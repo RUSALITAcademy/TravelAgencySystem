1637aa39912d15906f1e3b722dd6319583640338
HEAL-SAIZ <varnakovt@mail.ru>
2023-12-23 04:30:34 +0700

    1. Исправил navbar 2. Добавил routing 3. Страница личного кабинета. 4. Страница настроек. 5. Страница документов

--- frontend/Angular_project/src/app/app-routing.module.ts
+++ frontend/Angular_project/src/app/app-routing.module.ts
@@ -3,13 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthPageComponent } from './Pages/Auth-page/auth-page.component';
 import { MainPageComponent } from './Pages/Main-page/main-page.component';
 import { TourPageComponent } from './Pages/Tour-page/tour-page.component';
-import { UserAccountPageComponent } from './Pages/User-account-page/user-account-page.component';
 import { PaymentPageComponent } from './Pages/Payment-page/payment-page.component';
+
+//личный кабинет
+import { UserAccountPageComponent } from './Pages/User-account-page/user-account-page.component';
+import { AccountSettingsComponent } from './Components/User-account-page-components/account-settings/account-settings.component';
+import { AccountTravelHistoryComponent } from './Components/User-account-page-components/account-travel-history/account-travel-history.component';
+import { AccountDocumentsComponent } from './Components/User-account-page-components/account-documents/account-documents.component';
+//личный кабинет
 
 const routes: Routes = [
 
   { path: "auth", component: AuthPageComponent },
-  { path: "account", component: UserAccountPageComponent },
+  {
+    path: "account", component: UserAccountPageComponent,
+    children: [
+      { path: '', redirectTo: 'account', pathMatch: 'full' },
+      { path: "settings", component: AccountSettingsComponent },
+      { path: "history", component: AccountTravelHistoryComponent },
+      { path: "documents", component: AccountDocumentsComponent },
+    ]
+  },
   { path: "main", component: MainPageComponent },
   { path: "tour/:id", component: TourPageComponent },
   { path: "payment/:id", component: PaymentPageComponent },
